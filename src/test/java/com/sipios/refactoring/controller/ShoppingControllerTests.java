792d1cb876571211f233861bafc5afd98e99da12
saadigui <31103970+saadigui@users.noreply.github.com>
2022-04-26 22:30:20 +0200

    fix test for controller

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -1,21 +1,46 @@
 package com.sipios.refactoring.controller;
 
 import com.sipios.refactoring.UnitTest;
+import com.sipios.refactoring.service.RoutingService;
 import com.sipios.refactoring.service.dto.Body;
 import com.sipios.refactoring.service.dto.Item;
+import com.sipios.refactoring.service.dto.TypeEnum;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-class ShoppingControllerTests extends UnitTest {
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-    @InjectMocks
-    private ShoppingController controller;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+class ShoppingControllerTests {
+
+
+    private static MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void prepare() {
+        mockMvc = MockMvcBuilders
+            .standaloneSetup()
+            .setControllerAdvice()
+            .build();
+    }
 
     @Test
-    void should_not_throw() {
+    void should_not_throw() throws Exception {
+
         Assertions.assertDoesNotThrow(
-            () -> controller.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
+            () -> mockMvc
+                .perform(
+                    post("/shopping").content(new Body(new Item[] {}, TypeEnum.STANDARD_CUSTOMER).toString())
+                )
         );
     }
 }
